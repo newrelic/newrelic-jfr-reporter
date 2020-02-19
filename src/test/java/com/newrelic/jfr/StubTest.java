a988d835064ef5dcd9ed3eb4920860fe79afd472
jplumb <jplumb@newrelic.com>
2020-02-19 14:22:30 -0800

    set up scaffolding

--- src/test/java/com/newrelic/jfr/StubTest.java
+++ src/test/java/com/newrelic/jfr/StubTest.java
@@ -1,7 +1,13 @@
 package com.newrelic.jfr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StubTest {
 
+    @Test
+    void testNothing(){
+      assertTrue(true);
+    }
 }
