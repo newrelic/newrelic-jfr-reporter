a4c7fa61c26729c376026d1e55e8da36fb03ffd4
jplumb <jplumb@newrelic.com>
2020-02-20 14:38:16 -0800

    bring in the contents

--- src/main/java/com/newrelic/jfr/CPULoadMapper.java
+++ src/main/java/com/newrelic/jfr/CPULoadMapper.java
@@ -1,5 +1,7 @@
 package com.newrelic.jfr;
 
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.metrics.Gauge;
 import com.newrelic.telemetry.metrics.Metric;
 import jdk.jfr.consumer.RecordedEvent;
 
@@ -7,8 +9,16 @@ import java.util.List;
 
 public class CPULoadMapper implements EventMapper {
 
+
     @Override
-    public List<? extends Metric> apply(RecordedEvent recordedEvent) {
-        return null;
+    public List<? extends Metric> apply(RecordedEvent ev) {
+        var timestamp = ev.getStartTime().toEpochMilli();
+        //TODO: Wire up base attributes that get propagated for all metrics
+        var attr = new Attributes();
+        return List.of(
+                new Gauge("jfr:CPULoad.jvmUser", ev.getDouble("jvmUser"), timestamp, attr),
+                new Gauge("jfr:CPULoad.jvmSystem", ev.getDouble("jvmSystem"), timestamp, attr),
+                new Gauge("jfr:CPULoad.machineTotal", ev.getDouble("machineTotal"), timestamp, attr)
+        );
     }
 }
