4ee79deac1dd1ce9ba87c9505005c29fc4406744
jplumb <jplumb@newrelic.com>
2020-02-24 16:18:29 -0800

    like var

--- src/test/java/com/newrelic/jfr/JfrStreamEventConsumerTest.java
+++ src/test/java/com/newrelic/jfr/JfrStreamEventConsumerTest.java
@@ -1,30 +1,27 @@
 package com.newrelic.jfr;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.metrics.Count;
 import com.newrelic.telemetry.metrics.Gauge;
-import com.newrelic.telemetry.metrics.Metric;
 import com.newrelic.telemetry.metrics.MetricBuffer;
-import java.util.List;
 import jdk.jfr.consumer.RecordedEvent;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 class JfrStreamEventConsumerTest {
 
   @Test
   void testApplyMetrics() throws NoSuchMethodException {
-    EventMapper eventMapper = mock(EventMapper.class);
-    MetricBuffer metricBuffer = mock(MetricBuffer.class);
-    RecordedEvent event = mock(RecordedEvent.class);
-    JfrStreamEventConsumer testClass = new JfrStreamEventConsumer(eventMapper, metricBuffer);
-    Metric countMetric = new Count("foo", 2.0, 30, 50, new Attributes());
-    Metric gaugeMetric = new Gauge( "bar", 4,566, new Attributes());
-    List<Metric> metrics = List.of(gaugeMetric, countMetric);
+    var eventMapper = mock(EventMapper.class);
+    var metricBuffer = mock(MetricBuffer.class);
+    var event = mock(RecordedEvent.class);
+    var testClass = new JfrStreamEventConsumer(eventMapper, metricBuffer);
+    var countMetric = new Count("foo", 2.0, 30, 50, new Attributes());
+    var gaugeMetric = new Gauge( "bar", 4,566, new Attributes());
+    var metrics = List.of(gaugeMetric, countMetric);
 
     doReturn(metrics).when(eventMapper).apply(event);
     testClass.accept(event);
