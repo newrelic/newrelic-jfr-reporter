24cad922d44d7d68dbb95f12a475697bd3ce9fe4
jkeller <jkeller@newrelic.com>
2020-02-27 13:01:20 -0800

    JAVA-5709 Update JfrStreamEventConsumerTest

--- src/test/java/com/newrelic/jfr/JfrStreamEventConsumerTest.java
+++ src/test/java/com/newrelic/jfr/JfrStreamEventConsumerTest.java
@@ -1,5 +1,6 @@
 package com.newrelic.jfr;
 
+import com.newrelic.jfr.attributes.BaseAttributes;
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.metrics.Count;
 import com.newrelic.telemetry.metrics.Gauge;
@@ -19,8 +20,8 @@ class JfrStreamEventConsumerTest {
     var metricBuffer = mock(MetricBuffer.class);
     var event = mock(RecordedEvent.class);
     var testClass = new JfrStreamEventConsumer(eventMapper, metricBuffer);
-    var countMetric = new Count("foo", 2.0, 30, 50, new Attributes());
-    var gaugeMetric = new Gauge( "bar", 4,566, new Attributes());
+    var countMetric = new Count("foo", 2.0, 30, 50, new BaseAttributes().get());
+    var gaugeMetric = new Gauge( "bar", 4,566, new BaseAttributes().get());
     var metrics = List.of(gaugeMetric, countMetric);
 
     doReturn(metrics).when(eventMapper).apply(event);
