b9334a17e1544e5582e734cd54db7554266dac41
jkeller <jkeller@newrelic.com>
2020-02-26 17:34:11 -0800

    JAVA-5709 Add LinkingMetadata

--- src/main/java/com/newrelic/jfr/Entrypoint.java
+++ src/main/java/com/newrelic/jfr/Entrypoint.java
@@ -2,13 +2,15 @@ package com.newrelic.jfr;
 
 import com.newrelic.api.agent.Logger;
 import com.newrelic.api.agent.NewRelic;
+import com.newrelic.jfr.attributes.LinkingMetadata;
 import com.newrelic.telemetry.Attributes;
 
 import java.lang.instrument.Instrumentation;
-import java.net.InetAddress;
 import java.net.URI;
-import java.net.UnknownHostException;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
+
+import static com.newrelic.jfr.attributes.LinkingMetadata.*;
 
 public class Entrypoint {
     public static void premain(String agentArgs, Instrumentation inst) {
@@ -16,39 +18,42 @@ public class Entrypoint {
         logger.log(Level.INFO, "Attaching New Relic JFR Monitor");
         String appName = NewRelic.getAgent().getConfig().getValue("app_name");
 
-        try {
-            var commonAttributes = new Attributes()
-                    .put("host", getHostName())
-                    .put("appName", appName)
-                    .put("service.name", appName)
-                    .put("instrumentation.name", "JFR")
-                    .put("instrumentation.provider", "JFR Agent Extension")
-                    .put("collector.name", "JFR Agent Extension");
+        // This needs to be on a different thread or else it blocks the agent initialization
+        Executors.newSingleThreadScheduledExecutor().submit(() -> {
+            try {
+                waitForAgentInitialization(logger);
 
-            var agentConfig = NewRelic.getAgent().getConfig();
-            String insertApiKey = agentConfig.getValue("insert_api_key");
-            String metricIngestUri = agentConfig.getValue("metric_ingest_uri");
+                var commonAttributes = new Attributes()
+                        .put("host", getHostname())
+                        .put(HOSTNAME, getHostname())
+                        .put(ENTITY_GUID, getEntityGuid())
+                        .put(ENTITY_NAME, getEntityName())
+                        .put(ENTITY_TYPE, getEntityType())
+                        .put(SPAN_ID, getSpanId())
+                        .put(TRACE_ID, getTraceId())
+                        .put("appName", appName)
+                        .put("service.name", appName)
+                        .put("instrumentation.name", "JFR")
+                        .put("instrumentation.provider", "JFR Agent Extension")
+                        .put("collector.name", "JFR Agent Extension");
 
-            var builder = Config.builder()
-                    .insertApiKey(insertApiKey)
-                    .commonAttributes(commonAttributes)
-                    .logger(logger);
+                var agentConfig = NewRelic.getAgent().getConfig();
+                String insertApiKey = agentConfig.getValue("insert_api_key");
+                String metricIngestUri = agentConfig.getValue("metric_ingest_uri");
 
-            if ((metricIngestUri != null) && (!metricIngestUri.isEmpty())) {
-                builder = builder.metricsIngestUri(URI.create(metricIngestUri));
+                var builder = Config.builder()
+                        .insertApiKey(insertApiKey)
+                        .commonAttributes(commonAttributes)
+                        .logger(logger);
+
+                if ((metricIngestUri != null) && (!metricIngestUri.isEmpty())) {
+                    builder = builder.metricsIngestUri(URI.create(metricIngestUri));
+                }
+                var config = builder.build();
+                new Reporter(config).start();
+            } catch (Throwable t) {
+                logger.log(Level.SEVERE, t, "Unable to attach New Relic JFR Monitor");
             }
-            var config = builder.build();
-            new Reporter(config).start();
-        } catch (Throwable t) {
-            logger.log(Level.SEVERE, t, "Unable to attach New Relic JFR Monitor");
-        }
-    }
-
-    static String getHostName() {
-        try {
-            return InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            return "unknown";
-        }
+        });
     }
 }
---
+++ src/main/java/com/newrelic/jfr/attributes/LinkingMetadata.java
@@ -0,0 +1,73 @@
+package com.newrelic.jfr.attributes;
+
+import com.newrelic.api.agent.Logger;
+import com.newrelic.api.agent.NewRelic;
+
+import java.util.Map;
+import java.util.logging.Level;
+
+/**
+ * A map of linking metadata returned by the agent potentially containing the following:
+ * trace.id, span.id, entity.name, entity.type, entity.guid, hostname
+ */
+public class LinkingMetadata {
+    public static final String TRACE_ID = "trace.id";
+    public static final String SPAN_ID = "span.id";
+    public static final String ENTITY_NAME = "entity.name";
+    public static final String ENTITY_TYPE = "entity.type";
+    public static final String ENTITY_GUID = "entity.guid";
+    public static final String HOSTNAME = "hostname";
+
+    /**
+     * Gets an opaque map of key/value pairs that can be used to correlate this application in the New Relic backend.
+     * Will throw a NullPointerException if RPMService is not yet running.
+     *
+     * @return Map<String, String> of linking metadata
+     */
+    public static Map<String, String> getLinkingMetadata() {
+        return NewRelic.getAgent().getLinkingMetadata();
+    }
+
+    public static boolean agentHasLinkingMetadata() {
+        try {
+            return !getLinkingMetadata().isEmpty();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void waitForAgentInitialization(Logger logger) throws InterruptedException {
+        while (true) {
+            if (agentHasLinkingMetadata()) {
+                logger.log(Level.INFO, "Java Agent now has linking data, JFR Extension can start.");
+                break;
+            }
+            Thread.sleep(1000);
+            logger.log(Level.INFO, "JFR Extension is waiting for the agent to finish connecting");
+        }
+    }
+
+    public static String getTraceId() {
+        return getLinkingMetadata().getOrDefault(TRACE_ID, "");
+    }
+
+    public static String getSpanId() {
+        return getLinkingMetadata().getOrDefault(SPAN_ID, "");
+    }
+
+    public static String getEntityName() {
+        return getLinkingMetadata().getOrDefault(ENTITY_NAME, "");
+    }
+
+    public static String getEntityType() {
+        return getLinkingMetadata().getOrDefault(ENTITY_TYPE, "");
+    }
+
+    public static String getEntityGuid() {
+        return getLinkingMetadata().getOrDefault(ENTITY_GUID, "");
+    }
+
+    public static String getHostname() {
+        return getLinkingMetadata().getOrDefault(HOSTNAME, "");
+    }
+}
