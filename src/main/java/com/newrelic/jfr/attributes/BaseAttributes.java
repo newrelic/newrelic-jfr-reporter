d5ca8dd6ce6e016f5d0f9e2f19035c9a35f10cc3
jkeller <jkeller@newrelic.com>
2020-02-28 14:00:00 -0800

    JAVA-5709 Omit traceId and spanId from base attrs for time being.

--- src/main/java/com/newrelic/jfr/attributes/BaseAttributes.java
+++ src/main/java/com/newrelic/jfr/attributes/BaseAttributes.java
@@ -4,17 +4,27 @@ import com.newrelic.telemetry.Attributes;
 
 import java.util.function.Supplier;
 
-import static com.newrelic.jfr.attributes.AttributeNames.SPAN_ID;
-import static com.newrelic.jfr.attributes.AttributeNames.TRACE_ID;
-import static com.newrelic.jfr.attributes.LinkingMetadata.getSpanId;
-import static com.newrelic.jfr.attributes.LinkingMetadata.getTraceId;
+//import static com.newrelic.jfr.attributes.AttributeNames.SPAN_ID;
+//import static com.newrelic.jfr.attributes.AttributeNames.TRACE_ID;
+//import static com.newrelic.jfr.attributes.LinkingMetadata.getSpanId;
+//import static com.newrelic.jfr.attributes.LinkingMetadata.getTraceId;
 
 public class BaseAttributes implements Supplier<Attributes> {
     @Override
     public Attributes get() {
-        return new Attributes()
-                // TODO do we want to add span.id and trace.id to each metric? Should we omit them if they're empty?
-                .put(SPAN_ID, getSpanId())
-                .put(TRACE_ID, getTraceId());
+        Attributes baseAttributes = new Attributes();
+        // TODO do we want to add span.id and trace.id to each metric? Use case?
+//        String spanId = getSpanId();
+//        String traceId = getTraceId();
+//
+//        if (spanId != null && !spanId.isEmpty()) {
+//            baseAttributes.put(SPAN_ID, spanId);
+//        }
+//
+//        if (traceId != null && !traceId.isEmpty()) {
+//            baseAttributes.put(TRACE_ID, traceId);
+//        }
+
+        return baseAttributes;
     }
 }
--- src/test/java/com/newrelic/jfr/attributes/BaseAttributesTest.java
+++ src/test/java/com/newrelic/jfr/attributes/BaseAttributesTest.java
@@ -3,8 +3,8 @@ package com.newrelic.jfr.attributes;
 import com.newrelic.telemetry.Attributes;
 import org.junit.jupiter.api.Test;
 
-import static com.newrelic.jfr.attributes.AttributeNames.SPAN_ID;
-import static com.newrelic.jfr.attributes.AttributeNames.TRACE_ID;
+//import static com.newrelic.jfr.attributes.AttributeNames.SPAN_ID;
+//import static com.newrelic.jfr.attributes.AttributeNames.TRACE_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BaseAttributesTest {
@@ -12,8 +12,8 @@ class BaseAttributesTest {
     @Test
     void testGet() {
         Attributes expectedAttributes = new Attributes();
-        expectedAttributes.put(TRACE_ID, "");
-        expectedAttributes.put(SPAN_ID, "");
+//        expectedAttributes.put(TRACE_ID, "");
+//        expectedAttributes.put(SPAN_ID, "");
         Attributes baseAttributes = new BaseAttributes().get();
 
         assertEquals(expectedAttributes, baseAttributes);
