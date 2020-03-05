75ee4d0769a523eb6635770c41a55da288b500ed
Xi Xia <xxia@newrelic.com>
2020-03-05 13:53:16 -0800

    create just one logger with the same name

--- src/main/java/com/newrelic/jfr/logger/AgentLoggerFactory.java
+++ src/main/java/com/newrelic/jfr/logger/AgentLoggerFactory.java
@@ -1,27 +1,22 @@
 package com.newrelic.jfr.logger;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 
 public class AgentLoggerFactory implements ILoggerFactory {
 
-  ConcurrentMap<String, Logger> loggerMap;
+  private static final String LOGGER_NAME = "Agent_SLF4J_Custom_Logger";
+  private AgentLoggerAdapter agentLoggerAdapter;
 
   public AgentLoggerFactory() {
-    loggerMap = new ConcurrentHashMap<String, Logger>();
   }
 
   @Override
   public Logger getLogger(String name) {
-    Logger agentLogger = loggerMap.get(name);
-    if (agentLogger != null) {
-      return agentLogger;
-    } else {
-      Logger newInstance = new AgentLoggerAdapter(name);
-      Logger oldInstance = loggerMap.putIfAbsent(name, newInstance);
-      return oldInstance == null ? newInstance : oldInstance;
+    if (agentLoggerAdapter != null) {
+      return agentLoggerAdapter;
     }
+    return new AgentLoggerAdapter(LOGGER_NAME);
   }
 }
+
