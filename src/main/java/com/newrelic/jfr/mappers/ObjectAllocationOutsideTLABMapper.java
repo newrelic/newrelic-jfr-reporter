65d317ad0d071886bbb3380b772974614d7e7ddb
jplumb <jplumb@newrelic.com>
2020-03-06 11:10:50 -0800

    remove try/catch experiment

--- src/main/java/com/newrelic/jfr/mappers/ObjectAllocationOutsideTLABMapper.java
+++ src/main/java/com/newrelic/jfr/mappers/ObjectAllocationOutsideTLABMapper.java
@@ -7,27 +7,20 @@ import jdk.jfr.consumer.RecordedEvent;
 
 import java.util.List;
 
-import static java.util.Collections.emptyList;
-
 public class ObjectAllocationOutsideTLABMapper implements EventMapper {
     public static final String EVENT_NAME = "jdk.ObjectAllocationOutsideTLAB";
 
     @Override
     public List<? extends Metric> apply(RecordedEvent ev) {
         var start = ev.getStartTime().toEpochMilli();
-        try {
-            var attr = new Attributes();
-            var threadName = Workarounds.getThreadName(ev);
-            threadName.ifPresent(t -> attr.put("thread.name", t));
+        var attr = new Attributes();
+        var threadName = Workarounds.getThreadName(ev);
+        threadName.ifPresent(t -> attr.put("thread.name", t));
 
-            return List.of(
-                    new Gauge("jfr:ObjectAllocationOutsideTLAB.allocation", 0.0 + ev.getLong("allocationSize"),
-                            start, attr)
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-            return emptyList();
-        }
+        return List.of(
+                new Gauge("jfr:ObjectAllocationOutsideTLAB.allocation", 0.0 + ev.getLong("allocationSize"),
+                        start, attr)
+        );
     }
 
     @Override
