737bd0373991eeff8f7b2caf05ccea872dcbc5d4
jplumb <jplumb@newrelic.com>
2020-03-06 11:12:23 -0800

    add comment for future selves.

--- src/main/java/com/newrelic/jfr/mappers/Workarounds.java
+++ src/main/java/com/newrelic/jfr/mappers/Workarounds.java
@@ -7,6 +7,12 @@ import java.util.Optional;
 
 public class Workarounds {
 
+    /**
+     * There are cases where the event has the wrong type inside it
+     * for the thread, so calling ev.getThread(name) internally
+     * throws a ClassCastException.  We work around it here by just
+     * getting the raw value and checking the type.
+     */
     static Optional<String> getThreadName(RecordedEvent ev) {
         Object thisField = ev.getValue("eventThread");
         if (thisField instanceof RecordedThread) {
